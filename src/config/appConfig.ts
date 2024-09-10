b8a15b49227a534bdfc08aff03870717a764d53d
itur-anaf <aman.itur@gmail.com>
2024-09-10 11:16:16 +0300

    change to localhost

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -2,7 +2,7 @@ const devMode = process.env.NODE_ENV === "development";
 
 const AppConfig = {
   devMode,
-  serverUrl: devMode ? "http://127.0.0.1:3001" : "http://dnet.idf",
+  serverUrl: devMode ? "http://localhost:3001" : "http://dnet.idf",
   appUrl: devMode ? "http://localhost:3000" : "https://app.ghostwrites.ai",
 };
 console.log(AppConfig);
